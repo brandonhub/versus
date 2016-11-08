376dc09fa7399b8bf9568c27891917009d73c16a
Brandon Meeks <meeby1030@gmail.com>
2016-11-08 16:04:29 -0600

    Initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'versus' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for versus
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+  
+end
---
+++ versus/Group.swift
@@ -0,0 +1,21 @@
+
+//
+//  Group.swift
+//  versus
+//
+//  Created by Brandon Meeks on 11/8/16.
+//  Copyright © 2016 meeks. All rights reserved.
+//
+
+import Foundation
+
+class Group {
+    
+    // will add more members later
+    var title:String
+    
+    init(title:String) {
+        self.title = title
+    }
+
+}
